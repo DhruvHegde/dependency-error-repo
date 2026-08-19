f8019c7f0222296a097e9442bf29dca455d4269f
Aayush Jha <aayushjha1105@gmail.com>
2026-08-19 17:07:45 +0530

    test(F3): inject test failure variant 60

--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
 import pytest
 
-def test_index_out_of_bounds_59():
-    data = [1, 2, 3]
-    val = data[802]
-    assert val > 0
+def test_assertion_mismatch_60():
+    expected = 221
+    actual = 4357
+    assert expected == actual, f"Expected {expected} but got {actual}"
