32b82e748682f4eec0ddfe4591dbcd5e2838c574
Aayush Jha <aayushjha1105@gmail.com>
2026-08-19 17:08:59 +0530

    test(F3): inject test failure variant 16

--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
 import pytest
 
-def test_missing_dict_key_15():
-    config = {"timeout": 30, "retries": 3}
-    val = config["missing_key_5220"]
-    assert val == True
+def test_index_out_of_bounds_16():
+    data = [1, 2, 3]
+    val = data[799]
+    assert val > 0
