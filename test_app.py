3896a6da6a55d5ebf7c36a7203a7327edf04922b
Aayush Jha <aayushjha1105@gmail.com>
2026-08-19 17:17:53 +0530

    test(F3): inject test failure variant 123

--- test_app.py
+++ test_app.py
@@ -1,6 +1,5 @@
 import pytest
 
-def test_index_out_of_bounds_122():
-    data = [1, 2, 3]
-    val = data[905]
-    assert val > 0
+def test_type_mismatch_123():
+    result = "string_value" + 326
+    assert result is not None
