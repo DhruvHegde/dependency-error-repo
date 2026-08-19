14017be010f7b2e9d73acbf9209a9bddb0c70369
Aayush Jha <aayushjha1105@gmail.com>
2026-08-19 17:15:50 +0530

    test(F3): inject test failure variant 100

--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
 import pytest
 
-def test_index_out_of_bounds_99():
-    data = [1, 2, 3]
-    val = data[448]
-    assert val > 0
+def test_missing_dict_key_100():
+    config = {"timeout": 30, "retries": 3}
+    val = config["missing_key_2498"]
+    assert val == True
