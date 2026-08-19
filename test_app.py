7a6acae6967c2743e3beabeab18e78d18000d75a
Aayush Jha <aayushjha1105@gmail.com>
2026-08-19 17:20:10 +0530

    test(F3): inject test failure variant 148

--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
 import pytest
 
-def test_assertion_mismatch_147():
-    expected = 552
-    actual = 6319
-    assert expected == actual, f"Expected {expected} but got {actual}"
+def test_index_out_of_bounds_148():
+    data = [1, 2, 3]
+    val = data[928]
+    assert val > 0
