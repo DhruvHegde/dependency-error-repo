2847b3050764452336a207a4b5bf07c36e752d68
Aayush Jha <aayushjha1105@gmail.com>
2026-08-19 17:04:32 +0530

    test(F3): inject test failure variant 21

--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
 import pytest
 
-def test_missing_dict_key_20():
-    config = {"timeout": 30, "retries": 3}
-    val = config["missing_key_5552"]
-    assert val == True
+def test_index_out_of_bounds_21():
+    data = [1, 2, 3]
+    val = data[597]
+    assert val > 0
