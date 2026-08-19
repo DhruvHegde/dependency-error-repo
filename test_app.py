a59c0f8f4d9282f19707d355164d208f6ebf3ec2
Aayush Jha <aayushjha1105@gmail.com>
2026-08-19 17:05:27 +0530

    test(F3): inject test failure variant 32

--- test_app.py
+++ test_app.py
@@ -1,5 +1,6 @@
 import pytest
 
-def test_type_mismatch_31():
-    result = "string_value" + 519
-    assert result is not None
+def test_index_out_of_bounds_32():
+    data = [1, 2, 3]
+    val = data[828]
+    assert val > 0
