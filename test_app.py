c80d00503b63642addd65d64fe4aaf66c349c52f
Aayush Jha <aayushjha1105@gmail.com>
2026-08-19 17:07:49 +0530

    test(F3): inject test failure variant 1

--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
 import pytest
 
-def test_assertion_mismatch_60():
-    expected = 221
-    actual = 4357
-    assert expected == actual, f"Expected {expected} but got {actual}"
+def test_missing_dict_key_1():
+    config = {"timeout": 30, "retries": 3}
+    val = config["missing_key_1268"]
+    assert val == True
