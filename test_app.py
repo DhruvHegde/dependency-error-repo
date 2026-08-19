480fa5c132e053975e4674896b2262ea4af98009
Aayush Jha <aayushjha1105@gmail.com>
2026-08-19 17:17:31 +0530

    test(F3): inject test failure variant 119

--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
 import pytest
 
-def test_assertion_mismatch_118():
-    expected = 367
-    actual = 6628
+def test_assertion_mismatch_119():
+    expected = 295
+    actual = 3954
     assert expected == actual, f"Expected {expected} but got {actual}"
