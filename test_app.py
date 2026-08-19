d3b6ac83640d48bce0194fba103c6a47f3926d7b
Aayush Jha <aayushjha1105@gmail.com>
2026-08-19 17:10:53 +0530

    test(F3): inject test failure variant 40

--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
 import pytest
 
-def test_index_out_of_bounds_39():
-    data = [1, 2, 3]
-    val = data[373]
-    assert val > 0
+def test_assertion_mismatch_40():
+    expected = 115
+    actual = 1500
+    assert expected == actual, f"Expected {expected} but got {actual}"
