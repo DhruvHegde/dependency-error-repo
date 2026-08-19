f5b535f78491d5ec0d355b33095c8536fc9b197e
Aayush Jha <aayushjha1105@gmail.com>
2026-08-19 17:17:11 +0530

    test(F3): inject test failure variant 115

--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
 import pytest
 
-def test_missing_dict_key_114():
-    config = {"timeout": 30, "retries": 3}
-    val = config["missing_key_7274"]
-    assert val == True
+def test_index_out_of_bounds_115():
+    data = [1, 2, 3]
+    val = data[278]
+    assert val > 0
