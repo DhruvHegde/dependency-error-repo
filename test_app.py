98e24d9b30519441e2a7352911747ca7cf523bac
Aayush Jha <aayushjha1105@gmail.com>
2026-08-19 17:18:50 +0530

    test(F3): inject test failure variant 133

--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
 import pytest
 
-def test_index_out_of_bounds_132():
-    data = [1, 2, 3]
-    val = data[751]
-    assert val > 0
+def test_missing_dict_key_133():
+    config = {"timeout": 30, "retries": 3}
+    val = config["missing_key_1026"]
+    assert val == True
