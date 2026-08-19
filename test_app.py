df7f2bded749ec699dca2cc0d8837c58168b1717
Aayush Jha <aayushjha1105@gmail.com>
2026-08-19 17:11:20 +0530

    test(F3): inject test failure variant 46

--- test_app.py
+++ test_app.py
@@ -1,5 +1,6 @@
 import pytest
 
-def test_type_mismatch_45():
-    result = "string_value" + 448
-    assert result is not None
+def test_index_out_of_bounds_46():
+    data = [1, 2, 3]
+    val = data[420]
+    assert val > 0
