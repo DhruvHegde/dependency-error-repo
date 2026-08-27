a9439fe15ae64f6009b42a3939adedf7a7e7a09f
Aadarsh <aadarsha3128@gmail.com>
2026-08-27 10:38:09 +0530

    test(F4): inject timeout failure variant 417

--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
 import time
 
-def test_timeout_416():
-    counter = 655
-    while counter != 234135:
+def test_timeout_417():
+    counter = 486
+    while counter != 258217:
         counter += 1
