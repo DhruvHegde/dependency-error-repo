f71eee1e7541eb826586f87531920551b1de594d
Aadarsh <aadarsha3128@gmail.com>
2026-08-27 10:15:05 +0530

    test(F4): inject timeout failure variant 162

--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
 import time
 
-def test_timeout_161():
-    value = 484
-    while value < 666891:
-        value += 1
+def test_timeout_162():
+    counter = 829
+    while counter != 338576:
+        counter += 1
