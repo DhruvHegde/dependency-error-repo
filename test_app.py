50ce78eb3827df1db8af048b07b2669c1e9391d1
Aadarsh <aadarsha3128@gmail.com>
2026-08-27 10:32:36 +0530

    test(F4): inject timeout failure variant 356

--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
 import time
 
-def test_timeout_355():
-    counter = 808
-    while counter != 793534:
-        counter += 1
+def test_timeout_356():
+    value = 705
+    while value < 138625:
+        value += 1
