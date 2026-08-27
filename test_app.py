a2058bb96ec3c50c7dc54c89425564861f3f622f
Aadarsh <aadarsha3128@gmail.com>
2026-08-27 10:25:34 +0530

    test(F4): inject timeout failure variant 278

--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
 import time
 
-def test_timeout_277():
-    counter = 633
-    while counter != 789887:
+def test_timeout_278():
+    counter = 655
+    while counter != 924838:
         counter += 1
