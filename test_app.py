9b4c97043ef8fc71f4ea4b931201fb9302e6697d
Aadarsh <aadarsha3128@gmail.com>
2026-08-27 10:11:13 +0530

    test(F4): inject timeout failure variant 119

--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
 import time
 
-def test_timeout_118():
-    counter = 593
-    while counter != 407419:
-        counter += 1
+def test_timeout_119():
+    value = 200
+    while value < 109689:
+        value += 1
