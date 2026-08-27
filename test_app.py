b6fb12a9c15bb6bbb6d2a277d09f75caf981e50a
Aadarsh <aadarsha3128@gmail.com>
2026-08-27 10:01:25 +0530

    test(F4): inject timeout failure variant 10

--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
 import time
 
-def test_timeout_9():
-    counter = 113
-    while counter != 608627:
-        counter += 1
+def test_timeout_10():
+    value = 469
+    while value < 902133:
+        value += 1
